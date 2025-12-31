0db5d04fee6ae83d5a1e9a1f579742afd9deeaef
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 <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>
2025-12-31 18:42:40 +0200

    last

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,16 @@ Route::post('/trial', [TrialController::class, 'store'])->name('trial.store');
 Route::get('/language/{locale}', function ($locale) {
     if (in_array($locale, ['en', 'ar'])) {
         session(['locale' => $locale]);
+        app()->setLocale($locale);
     }
-    return redirect()->back();
+    
+    // Try to redirect back, but fallback to home if it fails
+    $previousUrl = url()->previous();
+    if ($previousUrl && $previousUrl !== url()->current()) {
+        return redirect($previousUrl);
+    }
+    
+    return redirect()->route('home');
 })->name('language.switch');
 
 // Admin Routes
